2edd357673e0c18dd6b1cdd3da3d401acd69373a
Prexani <prexani.tech@gmail.com>
2026-03-12 15:42:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3767026080-1ea2d",
+  "appId": "1:50964321592:web:6e37481daed56550885dcd",
+  "apiKey": "AIzaSyDYuw33MSyvnUTxFFJfqY_rzAK3oer6fLo",
+  "authDomain": "studio-3767026080-1ea2d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "50964321592"
+};
