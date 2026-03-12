ef8226f7f92f8ed529067b9cf92152361e776137
Prexani <prexani.tech@gmail.com>
2026-03-12 15:59:25 +0000

    new update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import { FAQ } from "@/components/FAQ";
 import { InquiryForm } from "@/components/InquiryForm";
 import { Footer } from "@/components/Footer";
 
+
 export default function Home() {
   return (
     <main className="min-h-screen">
@@ -25,3 +26,4 @@ export default function Home() {
     </main>
   );
 }
+
