8a8e244b686b8c51f56db1d722caf88e83656564
mr. perfect <abrawat01@gmail.com>
2026-04-10 03:46:40 +0000

    returncode: 1stdout:> nextn@0.1.0 start> NODE_ENV=production node server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 
-const { createServer } = require('https')
+const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
+const hostname = '0.0.0.0'
 const port = process.env.PORT || 3000
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
@@ -21,6 +21,6 @@ app.prepare().then(() => {
     }
   }).listen(port, (err) => {
     if (err) throw err
-    console.log(`> Ready on https://${hostname}:${port}`)
+    console.log(`> Ready on http://${hostname}:${port}`)
   })
 })
